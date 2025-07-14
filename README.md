a0825019a1384352a1f60b433818efe8d2cb2e63
Sulagna Ghosh <sulagnag@Sulagnas-MacBook-Air.local>
2025-07-14 13:45:45 -0500

    cleaning all codes and results to include only the ones used in the paper - for Simulation only

--- README.md
+++ README.md
@@ -4,14 +4,13 @@ This is a repository of the codes used to get results for the paper "Stein's unb
 ## Folders 
 1. results: Contains specific .csv files along with .png files, used to run the results and get what we need, such as, 
     - no_covariates: All results involving SURE-PM for getting Table 1 and 2 in section 6.1; 
-    - scores: Results to check scores from different models and experiments; 
-    - xie_losses: Contains all .csv files to get figure 1 in section 6.2; 
-    - xie_plots: Contains all necessaru images used in section 6.2. 
+    - xie_losses: Contains all .csv files to get figure 1, 2 and 3 in section 6.2; 
+    - xie_plots: Contains all necessary images used in section 6.2. 
 
 ## .py files 
-1. bayes_risk_calculation: Numerical calculation of bayes risk for experiments with no closed form bayes risk; 
-2. experiment_nocovariates.py, main_nocovariates.py, submitit_nocovariates.py and plot_results_nocovariates.R: Codes for comparing the models with no covariates (including SURE-PM), which give Table 1 and 2 in section 6.1; 
-3. experiments_xie.py, main_xie_checks, submitit_xie.py and plot_results_xie.R: All codes involving the experiments done involving covariates with eight different experiments and different models, which give Figure 1, 2 and 3 in Section 6.2; 
-4. models.py: Contains all the models (SURE-PM, SURE-THING, SURE-LS, EBCF and SURE-grandmean); 
-5. simulate_data.py: Codes to simulate data in different scenarios in section 6; 
-6. train.py: Functions to train different models (SURE-PM, SURE-THING, SURE-grandmean, SURE-LS, NPMLE and EBCF). 
+1. simulate_data.py: Codes to simulate data in different scenarios in section 6; 
+2. models.py: Contains all the models (SURE-PM, SURE-THING, SURE-LS, EBCF and SURE-grandmean); 
+3. train.py: Functions to train different models (SURE-PM, SURE-THING, SURE-grandmean, SURE-LS, NPMLE and EBCF); 
+4. experiment_nocovariates.py, main_nocovariates.py, submitit_nocovariates.py and plot_results_nocovariates.R: Codes for comparing the models with no covariates (including SURE-PM), which give Table 1 and 2 in section 6.1; 
+5. bayes_risk_calculation: Numerical calculation of bayes risk for xie experiments in Section 6.2 with no closed form bayes risk; 
+6. experiments_xie.py, main_xie_checks, submitit_xie.py and plot_results_xie.R: All codes involving the experiments done involving covariates with eight different experiments and different models, which give Figure 1, 2 and 3 in Section 6.2. 
--- plot_results_xie.R
+++ plot_results_xie.R
@@ -131,4 +131,351 @@ ggplot(df_mean_plot, aes(x = factor(n), color=model, shape=model, linetype=model
   ylab("In-sample MSE") +
   xlab("n")
 
-ggsave("results/xie_plots/median_scale_MSE_all_final_small.png", height=20, width=16)
+ggsave("results/xie_plots/median_scale_MSE_all_final_small.png", height=20, width=16) 
+
+# Read path ####
+
+read_path = paste("results/xie_losses/") 
+
+# Shrinkage / posterior means ####
+
+xie_shrinkage = read.csv(paste(read_path, "xie_shrinkage_location_scale.csv", sep="")) %>% 
+  select(-X) %>%
+  rename(thetaG = parametric_G, 
+         misspec_median_scale = EB_misspec_median_scale,
+         wellspec_median_scale = EB_wellspec_median_scale) %>%
+  select(experiment, Z, variance, NPMLE, thetaG, misspec_median_scale, wellspec_median_scale, truth) %>%
+  pivot_longer(4:8, names_to = "model", values_to = "posterior_mean") %>%
+  mutate(experiment = factor(experiment)) %>%
+  mutate(model_raw = model,
+         model = case_when(model == "misspec_median_scale" ~ "SURE-PM",
+                           model == "wellspec_median_scale" ~ "SURE-THING",
+                           model == "thetaG" ~ "SURE-grandmean",
+                           model == "truth" ~ "Ground truth",
+                           TRUE ~ model),
+         model = factor(model, levels = rev(c("NPMLE", "SURE-grandmean", "SURE-PM", "SURE-THING", "Ground truth"))),
+         experiment = factor(experiment, levels=c("c", "d", "f", "e"))) 
+
+# "truth" "SURE-PM" and NPMLE
+
+xie_shrinkage_e = xie_shrinkage %>% 
+  filter(experiment == "e")  %>%
+  mutate(variance = factor(variance)) %>% 
+  arrange(model)
+
+xie_shrinkage_e_thin = xie_shrinkage_e[seq(1, dim(xie_shrinkage_e)[1], 5), ]
+
+
+levels(xie_shrinkage_e_thin$variance) = c(TeX(r"($\sigma_i^2 = 0.1$)"),
+                                          TeX(r"($\sigma_i^2 = 0.5$)"))
+
+NPMLE_misspec_truth = dark2_palette[c(8, 3, 1)]
+NPMLE_misspec_truth[1] = "#9E9E9E"
+NPMLE_misspec_truth = setNames(NPMLE_misspec_truth, rev(c("NPMLE", "SURE-PM", "Ground truth")))
+
+marginals_df = read.csv(paste(read_path,"location_scale_marginals.csv", sep="")) %>% 
+  select(-X) 
+
+# 6400 n x 4 location scale x 5 experments x 3 variances
+marginals_df$use_scale = as.logical(marginals_df$use_scale)
+marginals_df$use_location = as.logical(marginals_df$use_location)
+
+marginals_med_scale = marginals_df %>% 
+  filter(use_scale, !use_location) %>%
+  filter(experiment != "d5") %>% 
+  pivot_longer(6:12, names_to = "model", values_to = "marginal") %>% 
+  filter(model != 'NPMLEinit') %>%
+  arrange(model) %>%
+  mutate(experiment = factor(experiment)) %>%
+  mutate(model_raw = model,
+         model = case_when(model == "misspec" ~ "SURE-PM",
+                           model == "wellspec" ~ "SURE-THING",
+                           model == "thetaG" ~ "SURE-grandmean",
+                           model == "truth" ~ "Ground truth",
+                           model == "surels" ~ "SURE-LS",
+                           TRUE ~ model),
+         # switch the order of models so that ground truth is in the back of the plot
+         model = factor(model, levels = c("Ground truth", "SURE-THING", "SURE-PM", "SURE-grandmean",  "NPMLE" )),
+         experiment = factor(experiment, levels=c("c", "d", "f", "e")))
+
+marginals_med_scale_thin = marginals_med_scale[seq(1, dim(marginals_med_scale)[1], 5), ]
+
+e_marginals_thin = marginals_med_scale_thin %>% 
+  filter(experiment == "e") %>%
+  mutate(variance = factor(variance))
+
+levels(e_marginals_thin$variance) = c(TeX(r"($\sigma_i^2 = 0.1$)"),
+                                      TeX(r"($\sigma_i^2 = 0.5$)"))
+
+# Marginals (e) with posterior means ####
+
+### Find quantiles of marginal ####
+
+alpha = 0.025
+
+find_alpha_quantiles_of_marginal = function(marginal_and_shrinkage_df, alpha=0.025){
+  
+  marginal = marginal_and_shrinkage_df %>%
+    filter(name == "B",  model == "Ground truth")  %>% # marginal
+    arrange(Z) %>% # order by Z
+    pull(value)
+  
+  Z = marginal_and_shrinkage_df %>%
+    filter(name == "B",  model == "Ground truth")  %>% # marginal
+    arrange(Z) %>% # order by Z
+    pull(Z)
+  
+  normalising_constant = sum(marginal)
+  
+  alpha_idx = max(which(cumsum(marginal)/normalising_constant <= alpha))
+  one_minus_alpha_idx = min(which(cumsum(marginal)/normalising_constant >= 1 - alpha))
+  
+  return(c(Z[alpha_idx], Z[one_minus_alpha_idx]))
+  
+}
+
+### 0.1 ####
+
+marginal_and_shrinkage_01 = xie_shrinkage_e_thin %>%
+  filter(variance =="sigma[i]^{\n    2\n} * {\n    phantom() == phantom()\n} * 0.1") %>%
+  filter(model_raw %in% c("truth", "misspec_median_scale", "NPMLE")) %>%
+  rename(value=posterior_mean) %>%
+  mutate(name="A") %>%
+  bind_rows(e_marginals_thin %>% 
+              select(-use_location, -use_scale) %>%
+              filter(variance =="sigma[i]^{\n    2\n} * {\n    phantom() == phantom()\n} * 0.1") %>%
+              filter(model_raw %in% c("truth", "misspec", "NPMLE")) %>%
+              rename(value=marginal) %>%
+              mutate(name="B"))
+
+find_alpha_quantiles_of_marginal(marginal_and_shrinkage_01)
+
+ribbon_marginal_01 = data.frame(Z = rep(find_alpha_quantiles_of_marginal(marginal_and_shrinkage_01, alpha=0.025), 1),
+                                y = c(1, 1),
+                                name = c("B", "B"))
+
+marginal_and_shrinkage_01 = marginal_and_shrinkage_01 %>%
+  mutate(alpha_Z_groundtruth = ribbon_marginal_01$Z[1],
+         one_minus_alpha_Z_grountruth = ribbon_marginal_01$Z[2],
+         alpha_95 = case_when(name == "A" & Z > one_minus_alpha_Z_grountruth ~ "transparency1", 
+                              name == "A" & Z < alpha_Z_groundtruth ~ "transparency2", 
+                              name == "A" ~ "solid",
+                              TRUE ~ NA))
+
+ggplot() +
+  geom_area(data=ribbon_marginal_01, aes(x=Z, y=y), fill="#9EEEEE", alpha=0.4) +
+  geom_line(data=marginal_and_shrinkage_01 %>%
+              mutate(delete_to_truncate = (name == "A" & value < 1)) %>% 
+              filter(Z >= -3, Z <= 3, !delete_to_truncate), aes(x=Z, y = value, color=model, linetype=model, alpha=alpha_95), size=0.8) +
+  facet_rep_grid(rows= vars(name), scales="free_y",
+                 labeller = as_labeller(c(B = "Marginal", `A` = "Posterior mean") ),
+                 switch="both",
+                 repeat.tick.labels = T) +
+  scale_color_manual(values=NPMLE_misspec_truth, name="", breaks=c("NPMLE", "SURE-PM", "Ground truth")) +
+  theme(legend.position="bottom", text=element_text(size=13),
+        legend.key.size = unit(3,"line"),
+        strip.background = element_blank(),
+        strip.placement = 'outside') +
+  ylab(TeX(r"($f_G(z \, | \, sigma_i^2)$)")) +
+  xlab(TeX("z")) +
+  scale_linetype_manual(values=c(2, 4, 1), name="", breaks=c("NPMLE", "SURE-PM", "Ground truth")) +
+  guides(alpha="none", linetype="none", color = "none") +
+  scale_alpha_manual(values=c(1, .5, .5)) +
+  ylab("")
+
+ggsave("results/xie_plots/marginal_posterior_1.png", width = 4, height=5)
+
+### 0.5 ####
+
+marginal_and_shrinkage_05 = xie_shrinkage_e_thin %>%
+  filter(variance =="sigma[i]^{\n    2\n} * {\n    phantom() == phantom()\n} * 0.5") %>%
+  filter(model_raw %in% c("truth", "misspec_median_scale", "NPMLE")) %>%
+  rename(value=posterior_mean) %>%
+  mutate(name="A") %>%
+  bind_rows(e_marginals_thin %>% 
+              select(-use_location, -use_scale) %>%
+              filter(variance =="sigma[i]^{\n    2\n} * {\n    phantom() == phantom()\n} * 0.5") %>%
+              filter(model_raw %in% c("truth", "misspec", "NPMLE")) %>%
+              rename(value=marginal) %>%
+              mutate(name="B"))
+
+ribbon_marginal_05 = data.frame(Z = find_alpha_quantiles_of_marginal(marginal_and_shrinkage_05, alpha=0.025),
+                                y = c(1, 1),
+                                name = c("B", "B"))
+
+marginal_and_shrinkage_05 = marginal_and_shrinkage_05 %>%
+  mutate(alpha_Z_groundtruth = ribbon_marginal_05$Z[1],
+         one_minus_alpha_Z_grountruth = ribbon_marginal_05$Z[2],
+         alpha_95 = case_when( name == "A" & Z > one_minus_alpha_Z_grountruth ~ "transparency1", 
+                               name == "A" & Z < alpha_Z_groundtruth ~ "transparency2", 
+                               name == "A" ~ "solid",
+                               TRUE ~ NA))
+
+
+
+ggplot() +
+  geom_area(data=ribbon_marginal_05, aes(x=Z, y=y), fill="#9EEEEE", alpha=0.4) +
+  geom_line(data=marginal_and_shrinkage_05  %>%
+              mutate(delete_to_truncate = (name == "A" & value < -1.5)) %>% 
+              filter(Z >= -3, Z <= 3, !delete_to_truncate), aes(x=Z, y = value, color=model, linetype=model, alpha=alpha_95), size=0.8) +
+  facet_rep_grid(rows= vars(name), scales="free_y",
+                 labeller = as_labeller(c(B = "Marginal", A = "Posterior mean") ),
+                 switch="both",
+                 repeat.tick.labels = T) +
+  scale_color_manual(values=NPMLE_misspec_truth, name="", breaks=c("NPMLE", "SURE-PM", "Ground truth")) +
+  theme(legend.position="bottom", text=element_text(size=13),
+        legend.key.size = unit(3,"line"),
+        strip.background = element_blank(),
+        strip.placement = 'outside') +
+  ylab(TeX(r"($f_G(z \, | \, sigma_i^2)$)")) +
+  xlab(TeX("z")) +
+  scale_linetype_manual(values=c(2, 4, 1), name="", breaks=c("NPMLE", "SURE-PM", "Ground truth")) +
+  guides(alpha="none",linetype="none", color = "none") +
+  scale_alpha_manual(values=c(1, .25, .25)) +
+  ylab("") +
+  geom_point(data=data.frame(x = c(0, 0), y = c(-1, 3), name= "A"),
+             aes(x=x, y=y), alpha=0)
+
+ggsave("results/xie_plots/marginal_posterior_5.png", width = 4, height=5) 
+
+## Combined 0.1 and 0.5 #### 
+
+combined_marginal_and_shrinkage = marginal_and_shrinkage_01 %>%
+  mutate(variance = 'low') %>% 
+  bind_rows((marginal_and_shrinkage_05 %>% mutate(variance = 'high'))) %>%
+  mutate(variance = factor(variance),
+         name = factor(name),
+         plotting_trick = ifelse(name == "A", "`Posterior mean`", "Marginal"),
+         plotting_trick = factor(plotting_trick, levels = c("`Posterior mean`", "Marginal")))
+
+levels(combined_marginal_and_shrinkage$variance) = c(low = TeX(r"($\sigma_i^2 = 0.1$)"),
+                                                     high = TeX(r"($\sigma_i^2 = 0.5$)"))
+
+
+combined_ribbon_marginal = ribbon_marginal_05 %>% mutate(variance = 'high') %>%
+  bind_rows((ribbon_marginal_01 %>% mutate(variance = 'low'))) %>%
+  mutate(variance = factor(variance),
+         plotting_trick = "Marginal",
+         plotting_trick = factor(plotting_trick, levels = c("`Posterior mean`", "Marginal")))
+
+levels(combined_ribbon_marginal$variance) = c(low = TeX(r"($\sigma_i^2 = 0.1$)"),
+                                              high = TeX(r"($\sigma_i^2 = 0.5$)"))
+
+
+
+ggplot() +
+  geom_area(data=combined_ribbon_marginal, aes(x=Z, y=y), fill="#9EEEEE", alpha=0.4) +
+  geom_line(data=combined_marginal_and_shrinkage %>% 
+              mutate(keep_posterior_mean_to_truncate_plot = 
+                       !( variance == "sigma[i]^{\n    2\n} * {\n    phantom() == phantom()\n} * 0.5" & name == "A" & value < 1)) %>% 
+              filter(keep_posterior_mean_to_truncate_plot, Z >= -3, Z <= 3),
+            aes(x=Z, y = value, color=model, linetype=model, alpha=alpha_95), size=0.8) +
+  facet_rep_grid(rows= vars(plotting_trick), cols = vars(variance), scales="free",
+                 labeller = label_parsed,
+                 switch="y",
+                 repeat.tick.labels = T) +
+  # facet_wrap(plotting_trick ~ variance, scales="free_y",
+  #                labeller = label_parsed) +
+  scale_color_manual(values=NPMLE_misspec_truth, name="", breaks=c("NPMLE", "SURE-PM", "Ground truth")) +
+  theme(legend.position="bottom", text=element_text(size=13),
+        legend.key.size = unit(3,"line"),
+        strip.background = element_blank(),
+        strip.placement = 'outside') +
+  ylab(TeX(r"($f_G(z \, | \, sigma_i^2)$)")) +
+  xlab(TeX("z")) +
+  scale_linetype_manual(values=c(2, 4, 1), name="", breaks=c("NPMLE", "SURE-PM", "Ground truth")) +
+  guides(alpha="none") +
+  scale_alpha_manual(values=c(1, .25, .25)) +
+  ylab("") 
+
+ggsave("results/xie_plots/combined_marginal_shrinkage_truncate_axes.png", width = 7, height=5)
+
+
+ggplot() +
+  geom_area(data=combined_ribbon_marginal, aes(x=Z, y=y), fill="#9EEEEE", alpha=0.4) +
+  geom_line(data=combined_marginal_and_shrinkage,
+            aes(x=Z, y = value, color=model, linetype=model, alpha=alpha_95), size=0.8) +
+  facet_rep_grid(rows= vars(plotting_trick), cols = vars(variance), scales="free",
+                 labeller = label_parsed,
+                 switch="y",
+                 repeat.tick.labels = T) +
+  scale_color_manual(values=NPMLE_misspec_truth, name="", breaks=c("NPMLE", "SURE-PM", "Ground truth")) +
+  theme(legend.position="bottom", text=element_text(size=13),
+        legend.key.size = unit(3,"line"),
+        strip.background = element_blank(),
+        strip.placement = 'outside') +
+  ylab(TeX(r"($f_G(z \, | \, sigma_i^2)$)")) +
+  xlab(TeX("z")) +
+  scale_linetype_manual(values=c(2, 4, 1), name="", breaks=c("NPMLE", "SURE-PM", "Ground truth")) +
+  guides(alpha="none") +
+  scale_alpha_manual(values=c(1, .25, .25)) +
+  ylab("") 
+
+ggsave("results/xie_plots/combined_marginal_shrinkage_whole_axes.png", width = 7, height=5)
+
+## Priors (e) ####
+
+priors_raw =  read.csv(paste(read_path, "location_scale_priors.csv", sep="")) %>%
+  select(-X)
+
+priors = priors_raw %>%
+  select(ends_with("e_median_scale")) %>% 
+  pivot_longer(1:6) %>%
+  mutate(prior_variable = case_when(str_detect(name, "theta_grid") ~ "theta",
+                                    TRUE ~ "pi"),
+         experiment = case_when(str_detect(name, "_c_") ~ "c",
+                                str_detect(name, "_d_") ~ "d",
+                                str_detect(name, "_d5_") ~ "d5",
+                                str_detect(name, "_e_") ~ "e",
+                                str_detect(name, "_f_") ~ "f",),
+         model = case_when(str_detect(name, "misspec") ~ "misspec",
+                           str_detect(name, "NPMLEinit") ~ "NPMLEinit",
+                           str_detect(name, "NPMLE") ~ "NPMLE")) %>%
+  select(-name) %>%
+  pivot_wider(names_from = prior_variable, values_from = value, values_fn = list) %>%
+  unnest(cols=c(theta, pi))
+
+
+prior_e = priors %>%
+  filter(experiment == "e", model != "NPMLEinit") %>%
+  mutate(model = case_when(model == "misspec" ~ "SURE-PM",
+                           model == "wellspec" ~ "SURE-THING",
+                           model == "thetaG" ~ "SURE-grandmean",
+                           TRUE ~ model), 
+         model = factor(model, levels = c("NPMLE","SURE-PM","SURE-grandmean")),
+         plotting_trick = factor(model,levels = c("NPMLE","SURE-PM","SURE-grandmean")))
+
+theta_truth_01 = seq(min(prior_e$theta), max(prior_e$theta), by = 0.01)
+theta_truth_05 = seq(min(prior_e$theta), max(prior_e$theta), by = 0.01)
+pi_truth_01 = dnorm(theta_truth_01, mean=2, sd=sqrt(0.1)) # low variance
+pi_truth_05 = dnorm(theta_truth_05, mean=0, sd=sqrt(0.5)) # high variance
+ground_truth_e =  data.frame('experiment' = rep("e", 2*length(theta_truth_01)),
+                             'model' = factor(c(rep("low", length(theta_truth_01)),
+                                                rep("high", length(theta_truth_01)))),
+                             theta=c(theta_truth_01, theta_truth_05),
+                             pi=c(pi_truth_01, pi_truth_05))
+
+levels(ground_truth_e$model) = c(low = TeX(r"(T $\sigma^2_i = 0.1$)"),
+                                 high = TeX(r"(T $\sigma^2_i = 0.5$)"))
+
+
+
+levels(ground_truth_e$model) = c(low = TeX(r"($\sigma^2_i = 0.1$)"),
+                                 high = TeX(r"($\sigma^2_i = 0.5$)"))
+
+prior_e %>%
+  ggplot() +
+  geom_line(data=ground_truth_e, aes(x = theta, y = pi, group = model, linetype=model), size=0.8) +
+  geom_point(aes(x = theta, y = pi, color=model), size=2, alpha=0.9)  + 
+  geom_segment(aes(x = theta, y = pi, color=model, xend=theta,yend=pi - pi), alpha=0.9, size = 1) + 
+  facet_wrap(~ plotting_trick, labeller = label_parsed) +
+  scale_color_manual(values=c(dark2_palette[c(1, 3)], "#2E2E2E",  "#2E2E2E"), name="") +
+  scale_linetype_manual(values=c("dotted", "dashed"), name="", labels = parse_format()) +
+  theme(legend.position="bottom",
+        text=element_text(size=24), legend.margin=margin(),
+        legend.key.size = unit(3,"line")) +
+  xlab(TeX("$theta_j$")) +
+  ylab(TeX("$pi_j")) +
+  scale_x_continuous(limit = c(-5, 5))
+
+ggsave("results/xie_plots/e_prior.png", height=6, width=10)
